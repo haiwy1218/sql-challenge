e018c4dda1754ff2bff5e594ecb3608b5ac4e82b
Elizabeth Schiffman <68922041+haiwy1218@users.noreply.github.com>
2020-11-02 15:14:53 -0500

    commented SQL DATABASE DEFINITION

--- SQL_DATABASE_DEFINITION.sql
+++ SQL_DATABASE_DEFINITION.sql
@@ -1,3 +1,4 @@
+--drop tables--
 drop table dept_emp;
 drop table dept_manager;
 drop table salaries;
@@ -5,12 +6,13 @@ drop table titles cascade;
 drop table employees;
 drop table departments;
 
+--create department table--
 create table departments(
 dept_no varchar (10) PRIMARY KEY,
 dept_name varchar(50) NOT NULL
 );
 
-
+--create dept_emp table--
 create table dept_emp(
 emp_no INTEGER NOT NULL,
 dept_no varchar (10) NOT NULL
@@ -18,7 +20,7 @@ dept_no varchar (10) NOT NULL
 
 
 
-
+-- create dept_manager table--
 create table dept_manager(
 dept_no varchar (10) NOT NULL,
 emp_no INTEGER NOT NULL
@@ -26,7 +28,7 @@ emp_no INTEGER NOT NULL
 
 
 
-
+--create employees table--
 create table employees(
 emp_no INTEGER PRIMARY KEY,
 emp_title varchar(10) NOT NULL,
@@ -37,7 +39,7 @@ sex varchar(50) NOT NULL,
 hire_date date NOT NULL
 );
 
-
+-- create salaries table--
 create table salaries(
 emp_no INTEGER PRIMARY KEY,
 salary varchar(50) NOT NULL
@@ -45,13 +47,13 @@ salary varchar(50) NOT NULL
 
 
 
-
+--create titles table--
 create table titles(
 title_id varchar (10) PRIMARY KEY,
 title varchar(50) NOT NULL
 );
 
-
+-- alter tables and put constraints--
 alter table dept_emp ADD CONSTRAINT pk_dept_emp PRIMARY KEY  (emp_no, dept_no);
 alter table dept_emp ADD CONSTRAINT fk_dept_emp_1 FOREIGN KEY (dept_no) REFERENCES departments(dept_no);
 alter table dept_emp ADD CONSTRAINT fk_dept_emp_2 FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
